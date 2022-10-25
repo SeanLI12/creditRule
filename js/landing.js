d7536149c07fef82c3d9204f0f46280f299bf8f9
SeanLI12 <larcenciel12@gmail.com>
2022-10-25 20:32:14 +0800

    tesst

---
+++ js/landing.js
@@ -0,0 +1,101 @@
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
+let lang = getParameterByName('lang').toUpperCase(); 
+let title;
+
+
+for(let i=0;i<localization["All Terms"].length;i++){
+   if(localization["All Terms"][i]._CMSKEY=="o_SPORTSRULES") {
+       
+	   title=localization["All Terms"][i][lang];
+       localization["All Terms"].splice(i, 1); 
+	   
+   }
+   if(localization["All Terms"][i]._CMSKEY=="o_POPULAR") {
+       document.getElementById("popularheader").innerHTML=localization["All Terms"][i][lang];
+       localization["All Terms"].splice(i, 1); 
+   }
+   if(localization["All Terms"][i]._CMSKEY=="o_ALLSPORTSATOZ") {
+       document.getElementById("atozheader").innerHTML=localization["All Terms"][i][lang];
+       localization["All Terms"].splice(i, 1); 
+   }
+}
+
+
+//loop popular
+for (let i=0;i<popularlist.length;i++){
+  let optName=popularlist[i];
+  localization["All Terms"].forEach((item,i)=>{
+    if(item._CMSKEY==optName){
+      let divNode = document.createElement("div");  
+      let innerdivNode = document.createElement("div");  
+      innerdivNode.innerHTML=item[lang]; 
+	  innerdivNode.addEventListener("click",()=>{
+		
+		var itemID = item["FILE"];	
+
+		const newLocation =
+		"./"+lang.toLowerCase()+"/rules.html?v=" + Date.now()+'&page='+itemID+'&lang='+lang;
+			window.location.href = newLocation;
+			window.parent.window.postMessage({
+				topic: "changeTitle",
+				data: {title:title,pageName:item[lang]},
+			  }, "*");
+
+	  })
+      divNode.appendChild(innerdivNode);
+      divNode.setAttribute("class", "items");
+      document.getElementById("popular").appendChild(divNode);
+      localization["All Terms"].splice(i, 1); 
+    }
+  });
+}
+//loop all
+for (let i=0;i<localization["All Terms"].length;i++){
+  
+
+  let divNode = document.createElement("div");  
+  let innerdivNode = document.createElement("div");  
+  innerdivNode.innerHTML=localization["All Terms"][i][lang]; 
+  innerdivNode.addEventListener("click",()=>{
+		
+	var itemID = localization["All Terms"][i]["FILE"];	
+
+	const newLocation =
+		"./"+lang.toLowerCase()+"/rules.html?v=" + Date.now()+'&page='+itemID+'&lang='+lang;
+		window.location.href = newLocation;
+		window.parent.window.postMessage({
+			topic: "changeTitle",
+			data: {title:title,pageName:localization["All Terms"][i][lang]},
+		  }, "*");
+
+  })
+  divNode.appendChild(innerdivNode);
+  divNode.setAttribute("class", "items");
+  document.getElementById("sportatoz").appendChild(divNode);
+  
+  
+}
+window.onload=()=>{
+	postMessageToSetHeight(document.getElementById("landing").scrollHeight);
+	window.parent.window.postMessage({
+		topic: "changeTitle",
+		data: {title:title,pageName:""},
+	  }, "*");
+
+  }
+function postMessageToSetHeight(msg) {
+	window.parent.window.postMessage({
+		topic: "changeContent",
+		data: msg,
+	  }, "*");
+  }
+
