78375a74d9c06c3957f842b9da2024cfcc07676a
khsamonte <khsamonte@gmail.com>
2023-12-28 19:52:49 +0800

    Updated theme changer

--- js/themeChanger.js
+++ js/themeChanger.js
@@ -3,19 +3,16 @@ let urlPas = new URLSearchParams(querySt);
 let rTheme=urlPas.get('theme');
 let page=urlPas.get('page');
 
-console.log("Theme changer test.")
 
 if(page!=null){
   if(rTheme!=null){
     let ctTheme=rTheme.toUpperCase();
-    console.log("Page !== null = " + ctTheme);
-    ctTheme=="DARK" ? document.querySelectorAll("link")[0].href=`../css/rules-style_${ctTheme}.css` : false;
+    ctTheme!=="LIGHT" ? document.querySelectorAll("link")[0].href=`../css/rules-style_${ctTheme}.css` : false;
   }
 }else{
   if(rTheme!=null){
     let ctTheme=rTheme.toUpperCase();
-    console.log("Page === null = " + ctTheme);
-    ctTheme=="DARK" ? document.querySelectorAll("link")[0].href=`../../css/rules-style_${ctTheme}.css` : false;
+    ctTheme!=="LIGHT" ? document.querySelectorAll("link")[0].href=`../../css/rules-style_${ctTheme}.css` : false;
   }
 
 }
